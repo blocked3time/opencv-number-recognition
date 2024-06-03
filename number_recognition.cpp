4d17f453dbd97322e856b1ddb910977711f80a59
blocked3time <wjddydrb89@kunsan.ac.kr>
2024-06-03 14:25:43 +0900

    Update number_recognition.cpp

--- number_recognition.cpp
+++ number_recognition.cpp
@@ -11,14 +11,14 @@ void mousecallback(int e, int x, int y, int f, void* u) {
     Rect c(Point(499, 199), Point(699, 299));
     Rect s(Point(499, 0), Point(699, 99));
     Rect l(Point(499, 99), Point(699, 199));
-    Rect ex(Point(399, 499), Point(699, 99));
+    Rect ex(Point(399, 399), Point(699, 499));
     switch (e) {
     case EVENT_LBUTTONDOWN:
         if (r.contains(Point(x, y))) {
             op = Point(x, y);
         }
         else if (c.contains(Point(x, y))) {
-            cout << "cear" << endl;
+            cout << "clear" << endl;
             img(Rect(Point(2, 2), Point(498, 498))).setTo(255);
         }
         else if (s.contains(Point(x, y))) {
@@ -50,7 +50,7 @@ void mousecallback(int e, int x, int y, int f, void* u) {
         break;
     case EVENT_MOUSEMOVE:
         if (f & EVENT_FLAG_LBUTTON && r.contains(Point(x, y))) {
-            line(img, op, Point(x, y), Scalar(0,0,0), 2);
+            line(img, op, Point(x, y), Scalar(0, 0, 0), 2);
             op = Point(x, y);
         }
         break;
@@ -60,14 +60,14 @@ void mousecallback(int e, int x, int y, int f, void* u) {
     imshow("img", img);
 }
 int main(void) {
-    Mat img(500, 700, CV_8UC3, Scalar(255,255,255));
+    Mat img(500, 700, CV_8UC3, Scalar(255, 255, 255));
     string s[] = { "Save","Load","Clear","Run","Exit" };
     line(img, Point(499, 0), Point(499, 499), 0, 2);
     rectangle(img, Rect(Point(0, 0), Point(img.cols, img.rows)), 0, 2);
     for (int i = 0; i < 5; i++) {
-        line(img, Point(499, i * 100), Point(699, i * 100), Scalar(0,0,0), 2);
-        Size sizetext = getTextSize(s[i], FONT_HERSHEY_COMPLEX, 2, 3,0);
-        putText(img, s[i], Point(499 + (200-sizetext.width)/2, i*100+ (100+sizetext.height)/2), FONT_HERSHEY_COMPLEX, 2, 0, 3);
+        line(img, Point(499, i * 100), Point(699, i * 100), Scalar(0, 0, 0), 2);
+        Size sizetext = getTextSize(s[i], FONT_HERSHEY_COMPLEX, 2, 3, 0);
+        putText(img, s[i], Point(499 + (200 - sizetext.width) / 2, i * 100 + (100 + sizetext.height) / 2), FONT_HERSHEY_COMPLEX, 2, 0, 3);
     }
     imshow("img", img);
     setMouseCallback("img", mousecallback, &img);
